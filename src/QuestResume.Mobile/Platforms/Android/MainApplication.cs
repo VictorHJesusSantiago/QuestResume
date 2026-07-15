65c92d68524a03250811b15ab2949a18a0f3db80
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-07-15 12:47:49 -0300

    feat: add MainActivity and MainApplication for Android platform setup

---
+++ src/QuestResume.Mobile/Platforms/Android/MainApplication.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace QuestResume.Mobile;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
