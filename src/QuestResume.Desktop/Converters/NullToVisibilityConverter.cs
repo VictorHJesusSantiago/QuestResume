1079819f777a3597168ba234c4e2a06839a1a02b
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-08-09 18:59:35 -0300

    refactor: remove excessive comments in NullToVisibilityConverter and resource strings for clarity

--- src/QuestResume.Desktop/Converters/NullToVisibilityConverter.cs
+++ src/QuestResume.Desktop/Converters/NullToVisibilityConverter.cs
@@ -4,11 +4,6 @@ using System.Windows.Data;
 
 namespace QuestResume.Desktop.Converters;
 
-/// <summary>
-/// Converts <c>null</c> (or an empty collection) to <see cref="Visibility.Collapsed"/> and any
-/// other value to <see cref="Visibility.Visible"/>. Used to hide the "Fontes" row of a
-/// <see cref="QuestResume.Desktop.ViewModels.ChatEntry"/> when it has no sources.
-/// </summary>
 public sealed class NullToVisibilityConverter : IValueConverter
 {
     public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
